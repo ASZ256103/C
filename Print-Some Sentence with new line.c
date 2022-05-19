585be5352a60de5c6ce3d96f0b5efa250b9e6954
Md Asaduzzaman <105591956+ASZ256103@users.noreply.github.com>
2022-05-19 18:40:23 +0600

    Add files via upload

---
+++ Print-Some Sentence with new line.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+
+	printf("My name is Md Asaduzzaman.\n");
+    printf("My father's name is Md Jaynul Abedin.\n");
+	printf("My mothers name is Mst Sabina Khatun.");
+
+
+	return 0;
+
+}
