e1569cbddb7fa8be145d85a7ca8a9a9f015c3d68
Md Asaduzzaman <105591956+ASZ256103@users.noreply.github.com>
2022-05-19 18:53:15 +0600

    Add files via upload

---
+++ C-Basic-Tecniques/Print-Some Sentence with new line & tab.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+
+	printf("My name is Md Asaduzzaman.\n \t");
+    printf("My father's name is Md Jaynul Abedin.\n \t \t");
+	printf("My mothers name is Mst Sabina Khatun.");
+
+
+	return 0;
+
+}
